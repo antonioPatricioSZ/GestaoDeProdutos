721a6e40acea2a4bc372e3b10dc451afd4397446
PatricioSouza <patriciozebra3@gmail.com>
2023-09-16 15:06:36 -0300

    feat: adicionando classes de entidades ao projeto

---
+++ src/Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Entities;
+
+public class Product : BaseEntity {
+
+    public Product() {
+        PriceSale = PricePurchase * 1.20m;
+    }
+
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal PricePurchase { get; set; }
+    public decimal PriceSale { get; set; } 
+    public long CategoryId { get; set; }
+
+}
---
+++ src/Domain/Enum/CategoryEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Enum;
+
+public enum CategoryEnum {
+
+    Beleza = 1,
+    Bebida = 2,
+    Comida = 3,
+    Higiene = 4,
+    Limpeza = 5,
+
+}
